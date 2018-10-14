097a5cdbaaf55f27335f14414a2ac7bec410ded8
jatin kumar <42140725+jamloya@users.noreply.github.com>
2018-10-14 23:41:11 +0530

    Update 16 packed to unpacked.asm

--- 16 packed to unpacked.asm
+++ 16 packed to unpacked.asm
@@ -1,7 +1,9 @@
 data segment
              
-             pack dd 15249685h,78961235h
-             result db ? 
+             var dd 15249685h,78961235h
+             temp db ? 
+             result db ?
+              
 data ends
 
 code segment
@@ -12,21 +14,37 @@ code segment
 
       mov ax,data 
       mov ds,ax  
-      mov bp,0
-      mov cx,8
-      mov si,0
-   
+      mov di,0
+      mov cx,4
+      mov si,4
+      lea bp,var
   un:
       mov ax,0
-      mov al, byte ptr pack[si]
+      mov al,byte ptr [bp][si]
       shl ax,4
       shr al,4
-      mov result[bp],al
-      mov result[bp+1],ah
-      inc si
-      add bp,2
-      loop un:
-      code ends
-
-
+      mov temp[di],al
+      mov temp[di+1],ah
+      add si,1
+      add di,2
+      loop un:  ;last 8 digits
+     
+      mov cx,4
+      mov si,0
+   un1:
+      mov ax,0
+      mov al,byte ptr var[si]
+      shl ax,4
+      shr al,4
+      mov temp[di],al
+      mov temp[di+1],ah
+      add si,1
+      add di,2
+      loop un1:   ; first 8 digits
+      mov si,15
+      mov di,0
+      mov cx,8
+      lea bp,temp
+     
+   code ends
   end start
